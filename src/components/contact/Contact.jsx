a717bddf843b6691791d417b119bd6c9398409f2
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-02-28 15:50:45 +0500

    done contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -29,7 +29,51 @@ function Contact() {
               </h6>
               <button>Browse our packages</button>
             </div>
-            <form></form>
+            <form>
+              <div className="inputs">
+                <div className="name">
+                  <label htmlFor="name">Full name</label>
+                  <input type="text" id="name" />
+                </div>
+
+                <div className="name">
+                  <label htmlFor="number">Phone number</label>
+                  <input type="number" id="number" />
+                </div>
+
+                <div className="name">
+                  <label htmlFor="Address">Address</label>
+                  <input type="address" id="Address" />
+                </div>
+
+                <div className="name">
+                  <label htmlFor="Email">Email</label>
+                  <input type="email" id="Email" />
+                </div>
+
+                <div className="name">
+                  <label htmlFor="Requested service">Requested service</label>
+                  <input type="text" id="Requested service" />
+                </div>
+
+                <div className="name">
+                  <label htmlFor="Day of service"> Day of service</label>
+                  <input type="text" id="Day of service" />
+                </div>
+
+                <div className="name">
+                  <label htmlFor="area">Add a note</label>
+                  <textarea
+                    name="area"
+                    id="area"
+                    cols="30"
+                    rows="10"
+                  ></textarea>
+                </div>
+
+                <button type="submit">Submit message</button>
+              </div>
+            </form>
           </div>
         </div>
       </section>
